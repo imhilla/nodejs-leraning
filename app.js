4655496a8fe874a3eaf6981d7a1f0310847f818d
imhilla <hillaryodhiambo282@gmail.com>
2021-01-27 15:20:44 +0300

    add gmail node.js files

--- app.js
+++ app.js
@@ -92,9 +92,9 @@
 // var qdata = q.query;
 // console.log(qdata.month);
 // console.log(qdata.year);
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
 
 // http.createServer(function(req, res) {
 //   var q = url.parse(req.url, true);
@@ -132,28 +132,77 @@ var fs = require('fs');
 // eventEmitter.emit('scream');
 
 // upload files
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
+// var http = require('http');
+// var formidable = require('formidable');
+// var fs = require('fs');
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:Users/Hillary/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function () {
-        if (err) throw err;
-        res.write('File uploaded and moved');
-        res.end();
-      });
-    });
-  } else {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+// http.createServer(function (req, res) {
+//   if (req.url == '/fileupload') {
+//     var form = new formidable.IncomingForm();
+//     form.parse(req, function (err, fields, files) {
+//       var oldpath = files.filetoupload.path;
+//       var newpath = 'C:Users/Hillary/' + files.filetoupload.name;
+//       fs.rename(oldpath, newpath, function () {
+//         if (err) throw err;
+//         res.write('File uploaded and moved');
+//         res.end();
+//       });
+//     });
+//   } else {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+//     res.write('<input type="file" name="filetoupload"><br>');
+//     res.write('<input type="submit">');
+//     res.write('</form>');
+//     return res.end();
+//   }
+// }).listen(8080);
+// var nodemailer = require(nodemailer);
+
+// var transporter = nodemailer.createTransporter({
+//   service: 'gmail',
+//   auth: {
+//     user: 'hillaryodhiambo282@gmail.com',
+//     pass: '',
+//   }
+// });
+
+// var mailOptions = {
+//   from: 'hillaryodhiambo282@gmail.com',
+//   to: 'hillaryodhiambo282@gmail.com',
+//   subject: 'This is a test',
+//   text: 'That was easy',
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//  if(error) {
+//    console.log(error);
+//  } else {
+//    console.log('Email sent: ' + info.response );
+//  }
+// });
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'hillaryodhiambo282@gmail.com',
+    pass: ''
   }
-}).listen(8080);
+});
+
+var mailOptions = {
+  from: 'hillaryodhiambo282@gmail.com',
+  to: 'hillaryodhiambo282@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
